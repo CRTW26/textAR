2681906d05fef30f69839a70abe4be8956e3998b
CRTW26 <worthington.connor@gmail.com>
2020-11-10 08:27:44 +0000

    Adds text to scene

--- textAR/textAR/ViewController.swift
+++ textAR/textAR/ViewController.swift
@@ -19,14 +19,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        let text = SCNText(string: "Example Text", extrusionDepth: 1)
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.green
+        text.materials = [material]
         
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
