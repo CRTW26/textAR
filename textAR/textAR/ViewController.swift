bb826ed35e7c20df9d1962a05bc8ac073cc39187
CRTW26 <worthington.connor@gmail.com>
2020-11-11 16:55:41 +0000

    adds custom text

--- textAR/textAR/ViewController.swift
+++ textAR/textAR/ViewController.swift
@@ -2,78 +2,40 @@
 //  ViewController.swift
 //  textAR
 //
-//  Created by Connor Worthington on 10/11/2020.
+//  Created by Connor Worthington on 11/11/2020.
 //
 
+import Foundation
 import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
-
-    @IBOutlet var sceneView: ARSCNView!
+class ViewController: UIViewController {
+ 
+    var text: String = ""
+    
+    @IBOutlet weak var textField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        let text = SCNText(string: "Example Text", extrusionDepth: 1)
-        
-        let material = SCNMaterial()
-        material.diffuse.contents = UIColor.green
-        text.materials = [material]
-        
-        let node = SCNNode()
-        node.position = SCNVector3(x: 0, y:0.02, z: -0.1)
-        node.scale = SCNVector3(x: 0.01, y: 0.01, z: 0.01)
-        node.geometry = text
-        
-        sceneView.scene.rootNode.addChildNode(node)
-        sceneView.autoenablesDefaultLighting = true
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
+    @IBAction func textEntered(_ sender: UITextField, forEvent event: UIEvent) {
+        text = sender.text ?? "error"
+//        let arView = ARViewController()
+        self.performSegue(withIdentifier: "textsegue", sender: self)
+//        arView.text = text ?? "error"
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        sceneView.session.pause()
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if segue.destination is ARViewController
+        {
+            let vc = segue.destination as! ARViewController
+            vc.text = text
+        }
     }
 
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
+
